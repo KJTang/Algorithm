53501a625c30ece8b9bfacea1522928d2addf614
KJTang <kaijietang@outlook.com>
2017-05-04 10:09:48 +0800

    correct func name

--- leetcode/46/solution.h
+++ leetcode/46/solution.h
@@ -1,16 +1,16 @@
-// solution: 12ms, 25 cases
+// solution: 9ms, 25 cases
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         std::vector<std::vector<int>> result;
         std::vector<bool> flag(nums.size(), true);
-        DFS(nums, result, flag);
+        RecursiveFunc(nums, result, flag);
         return result;
     }
 private:
     std::vector<int> cur_;
 
-    void DFS(const std::vector<int>& nums, std::vector<std::vector<int>>& result, std::vector<bool>& flag) {
+    void RecursiveFunc(const std::vector<int>& nums, std::vector<std::vector<int>>& result, std::vector<bool>& flag) {
         if (cur_.size() == nums.size()) {
             result.push_back(cur_);
             return;
@@ -19,7 +19,7 @@ private:
             if (flag[i]) {
                 cur_.push_back(nums[i]);
                 flag[i] = false;
-                DFS(nums, result, flag);
+                RecursiveFunc(nums, result, flag);
                 flag[i] = true;
                 cur_.pop_back();
             }
