ac80c1f83f90bdfa0a170ea23dec5569f7516100
KJTang <kaijietang@outlook.com>
2017-05-18 22:38:48 +0800

    leetcode 187

--- leetcode/187/solution.h
+++ leetcode/187/solution.h
@@ -1,28 +1,26 @@
-// solution: 79ms, 32 cases, http://yuanhsh.iteye.com/blog/2185976
+// solution: 62ms, 32 cases, http://yuanhsh.iteye.com/blog/2185976
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
+        std::vector<std::string> result;
         if (s.size() <= 10) {
-            return {};
+            return result;
         }
         
-        std::map<int, int>  substrs;
-        std::vector<std::string> result;
+        std::map<int, int> substrs;
         int str_mask = 0;
         // first 10 characters
         for (int i = 0; i != 10; ++i) {
             str_mask = (str_mask << 3) | (s[i] & 7);
         }
-        ++substrs[str_mask];
+        substrs.insert(std::make_pair(str_mask, 1));
         // other characters
         for (int i = 10; i != s.size(); ++i) {
             // get cur substr(s[i-9 to i])'s mask
             str_mask = ((str_mask << 3) | (s[i] & 7)) & 0x3fffffff;
-            auto it = substrs.find(str_mask);
-            if (it != substrs.end() && it->second == 1) {   // count == 1: make sure there's no duplicate
+            if (substrs[str_mask]++ == 1) {     // count == 1: make sure there's no duplicate
                 result.push_back(s.substr(i - 9, 10));
             }
-            ++substrs[str_mask];
         }
         return result;
     }
