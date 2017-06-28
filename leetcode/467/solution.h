4df2b714b602c40ef8b55e1a795d4ff697f1c01c
KJTang <kaijietang@outlook.com>
2017-06-28 10:00:13 +0800

    leetcode 467

--- leetcode/467/solution.h
+++ leetcode/467/solution.h
@@ -1,5 +1,6 @@
-// solution:    https://discuss.leetcode.com/topic/70654/c-concise-solution/2
-//              http://blog.csdn.net/mebiuw/article/details/53469963
+// solution: 13ms, 81 cases
+// https://discuss.leetcode.com/topic/70654/c-concise-solution/2
+// http://blog.csdn.net/mebiuw/article/details/53469963
 class Solution {
 public:
     int findSubstringInWraproundString(string p) {
@@ -10,7 +11,7 @@ public:
             if (i > 0 && p[i - 1] != (cur + 26 - 1) % 26 + 'a') {
                 len = 0;
             }
-            if (++len > letters[cur]) {     // if string end by (cur + 'a') whose length bigger than record
+            if (++len > letters[cur]) {     // if string's length bigger than record who is ended by (cur + 'a')
                 res += len - letters[cur];
                 letters[cur] = len;
             }
